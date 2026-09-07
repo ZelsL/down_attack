024ab7cb4c57047899adf1d2ce3c7158b0378581
Emerson Laureano <emersonlaureanonas@gmail.com>
2026-09-07 17:35:28 -0300

    build: add `jest.config.js`

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/$1",
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
+  testTimeout: 60000,
+};
