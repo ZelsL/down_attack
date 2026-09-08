c5b77cee4e7dde3f4ac0306ca33c0739708e863f
Emerson Laureano <emersonlaureanonas@gmail.com>
2026-09-08 15:39:26 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,17 +1,27 @@
-describe("Anonymous user", () => {
-  test("Retrieving current system status", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
-    const responseBody = await response.json();
+import orchestrator from "~/tests/orchestrator.js";
 
-    expect(response.status).toBe(200);
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+describe("GET /api/v1/status", () => {
+  describe("anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-    expect(responseBody.updated_at).toBeDefined();
-    expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-    expect(responseBody.dependencies.database.version).toBe("16.15");
-    expect(responseBody.dependencies.database.opened_connections).toBeDefined();
-    expect(responseBody.dependencies.database.opened_connections).toBe(1);
-    expect(responseBody.dependencies.database.max_connections).toBe(100);
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+      expect(responseBody.dependencies.database.version).toBeDefined();
+      expect(
+        responseBody.dependencies.database.opened_connections,
+      ).toBeDefined();
+      expect(responseBody.dependencies.database.version).toEqual("16.15");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
   });
 });
