e112dc97c35a622aa28f7ade610a71ac745efd18
Emerson Laureano <emersonlaureanonas@gmail.com>
2026-09-13 19:42:47 -0300

    fix: align Discord user object structure with real API

--- server/api/v1/mock/discord/api/users/@me/index.js
+++ server/api/v1/mock/discord/api/users/@me/index.js
@@ -15,25 +15,9 @@ export default controller.handle({
 
     return UserObject();
 
-    // Example Authorization Information
+    // Example Discord Return
 
-    // {
-    //     "application": {
-    //         "id": "159799960412356608",
-    //         "name": "AIRHORN SOLUTIONS",
-    //         "icon": "f03590d3eb764081d154a66340ea7d6d",
-    //         "description": "",
-    //         "hook": true,
-    //         "bot_public": true,
-    //         "bot_require_code_grant": false,
-    //         "verify_key": "c8cde6a3c8c6e49d86af3191287b3ce255872be1fff6dc285bdb420c06a2c3c8"
-    //     },
-    //     "scopes": [
-    //         "guilds.join",
-    //         "identify"
-    //     ],
-    //     "expires": "2021-01-23T02:33:17.017000+00:00",
-    //     "user": {
+    //     {
     //         "id": "268473310986240001",
     //         "username": "discord",
     //         "avatar": "f749bb0cbeeb26ef21eca719337d20f1",
@@ -41,7 +25,6 @@ export default controller.handle({
     //         "global_name": "Discord",
     //         "public_flags": 131072
     //     }
-    // }
 
     async function UserObject(discord_id, username) {
       const fakeUsername = username ? username : faker.internet.username();
@@ -49,27 +32,12 @@ export default controller.handle({
       const fakeAvatar = faker.string.alphanumeric(32);
 
       const fakeUserObject = {
-        application: {
-          id: "159799960412356608",
-          name: "Down Attack",
-          icon: "300ae0e41577b4ceb9cd41d2ee91f96a",
-          description: "",
-          hook: true,
-          bot_public: true,
-          bot_require_code_grant: false,
-          verify_key:
-            "c8cde6a3c8c6e49d86af3191287b3ce255872be1fff6dc285bdb420c06a2c3c8",
-        },
-        scopes: ["guilds.join", "identify"],
-        expires: new Date(Date.now() + 604800000).toISOString(),
-        user: {
-          id: fakeUserId,
-          username: fakeUsername,
-          avatar: fakeAvatar,
-          discriminator: "0",
-          global_name: "Discord",
-          public_flags: 131072,
-        },
+        id: fakeUserId,
+        username: fakeUsername,
+        avatar: fakeAvatar,
+        discriminator: "0",
+        global_name: "Discord",
+        public_flags: 131072,
       };
 
       return fakeUserObject;
