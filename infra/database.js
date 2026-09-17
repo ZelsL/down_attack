ebd7a12e8362667fe577482ce8adc1fcef6aeaca
Emerson Laureano <emersonlaureanonas@gmail.com>
2026-09-17 20:03:35 -0300

    fix(database): enable SSL for remote hosts and Vercel preview enviroments

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,25 @@
 import { Client } from "pg";
 import { ServiceError } from "./errors.js";
 
+function getSSL() {
+  if (process.env.POSTGRES_SSL === "false") {
+    return false;
+  }
+  if (process.env.POSTGRES_SSL === "true") {
+    return true;
+  }
+  if (
+    process.env.NODE_ENV === "production" ||
+    process.env.VERCEL_ENV ||
+    (process.env.POSTGRES_HOST &&
+      process.env.POSTGRES_HOST !== "localhost" &&
+      process.env.POSTGRES_HOST !== "127.0.0.1")
+  ) {
+    return true;
+  }
+  return false;
+}
+
 function getDatabaseCredentials() {
   return {
     host: process.env.POSTGRES_HOST,
@@ -8,7 +27,7 @@ function getDatabaseCredentials() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "production" ? true : false,
+    ssl: getSSL(),
   };
 }
 
